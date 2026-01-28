92cdba805f525eb35b43e96c53ae76a704205a6b
LeCimmerien <bgp.connan@gmail.com>
2026-01-28 01:54:25 +0100

    init

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank = True)
+
+    content_panels = Page.content_panels + ["intro"]
+
+
+class BlogPost(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + ["date", "intro", "body"]
---
+++ wyrd/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-6d&vn5ohzo_#1hh@1k-pb4u8!=$kw+ujgz+7x65ck3$7=s9v8r"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
