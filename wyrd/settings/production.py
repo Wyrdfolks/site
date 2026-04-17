7280a20b29ac4aec5ac0ddda46890c32bfc6481c
LeCimmerien <bgp.connan@gmail.com>
2026-04-17 23:50:43 +0200

    fix http headers

--- wyrd/settings/production.py
+++ wyrd/settings/production.py
@@ -14,6 +14,18 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "www.wyrdfolks.com"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_PRELOAD = False
+
+WAGTAILADMIN_BASE_URL = "https://www.wyrdfolks.com"
+
+
 # ManifestStaticFilesStorage is recommended in production, to prevent
 # outdated JavaScript / CSS assets being served from cache
 # (e.g. after a Wagtail upgrade).
