7fbf82e4825f13c2fae8e2e08575f08654c7fea5
LeCimmerien <bgp.connan@gmail.com>
2026-04-17 16:08:42 +0200

    prepare prod deploy

--- wyrd/settings/production.py
+++ wyrd/settings/production.py
@@ -9,7 +9,7 @@ DEBUG = False
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fest.wyrdfolks.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "www.wyrdfolks.com"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
