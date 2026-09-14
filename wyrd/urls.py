b8420743816b5b99da4f63be46001be52ab6b0d2
LeCimmerien <bgp.connan@gmail.com>
2026-09-14 14:56:19 +0200

    add robots.txt (#48)

--- wyrd/urls.py
+++ wyrd/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
+from django.http import HttpResponse
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
@@ -15,6 +16,13 @@ urlpatterns = [
     path("search/", search_views.search, name="search"),
 ]
 
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Disallow: /admin/",
+        "Disallow: /django-admin/",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
 
 if settings.DEBUG:
     from django.conf.urls.static import static
@@ -31,6 +39,7 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
+    path("robots.txt", robots_txt),
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
